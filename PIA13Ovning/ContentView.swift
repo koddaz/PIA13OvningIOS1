80c0d44ae8f9e85289fb3dfba9f544287acbe763
koddaz <Albert.niklasson@gmail.com>
2024-11-07 11:58:13 +0100

    Initial Commit

---
+++ PIA13Ovning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PIA13Ovning
+//
+//  Created by Albert Niklasson on 2024-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PIA13Ovning/PIA13OvningApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIA13OvningApp.swift
+//  PIA13Ovning
+//
+//  Created by Albert Niklasson on 2024-11-07.
+//
+
+import SwiftUI
+
+@main
+struct PIA13OvningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
